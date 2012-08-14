c18bc93de629af5080b1f7c4d5f10178ec45e9c8
Christopher Petersen <christopher.petersen@gmail.com>
2012-08-14 09:14:54 -0700

    Small refactor

--- lib/assaydepot.rb
+++ lib/assaydepot.rb
@@ -4,23 +4,4 @@ require "assaydepot/client"
 require "assaydepot/model"
 require "assaydepot/ware"
 require "assaydepot/provider"
-
-# Twitter.configure do |config|
-#   config.consumer_key = YOUR_CONSUMER_KEY
-#   config.consumer_secret = YOUR_CONSUMER_SECRET
-#   config.oauth_token = YOUR_OAUTH_TOKEN
-#   config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
-# end
-
-module AssayDepot
-  class << self
-    include AssayDepot::Configurable
-  end
-
-  # Delegate to a AssayDepot::Client
-  #
-  # @return [AssayDepot::Client]
-  def client
-    AssayDepot::Client.new(options)
-  end
-end
+require "assaydepot/core"
---
+++ lib/assaydepot/core.rb
@@ -0,0 +1,12 @@
+module AssayDepot
+  class << self
+    include AssayDepot::Configurable
+  end
+
+  # Delegate to a AssayDepot::Client
+  #
+  # @return [AssayDepot::Client]
+  def client
+    AssayDepot::Client.new(options)
+  end
+end
