69a4a907e289e8a1ccfded103c122fd2d4c672c3
Christopher Petersen <christopher.petersen@gmail.com>
2012-08-14 07:39:19 -0700

    Refactoring the client slightly (to make ruby motion support easier)

--- lib/assaydepot/client.rb
+++ lib/assaydepot/client.rb
@@ -8,11 +8,11 @@ module AssayDepot
     end
 
     def search_url(params={})
-      "#{AssayDepot.url}/#{@search_type}.json?#{params.collect { |k,v| "#{k}=#{v.to_s.gsub(" ","+")}"}.join("&")}"
+      "#{AssayDepot.url}/#{@search_type}.json"
     end
 
     def get_url(id, params={})
-      "#{AssayDepot.url}/#{@search_type}/#{id}.json?#{params.collect { |k,v| "#{k}=#{v.to_s.gsub(" ","+")}"}.join("&")}"
+      "#{AssayDepot.url}/#{@search_type}/#{id}.json"
     end
 
     def search(query, facets, params={})
@@ -21,12 +21,16 @@ module AssayDepot
       facets.map do |name,value|
         params["facets[#{name}][]"] = value
       end
-      JSON.parse(open(search_url(params)).read)
+      execute(search_url, params)
     end
 
     def get(id, params={})
       params["access_token"] = AssayDepot.access_token
-      JSON.parse(open(get_url(id, params)).read)
+      execute(get_url(id), params)
+    end
+
+    def execute(url, params)
+      JSON.parse(open("#{url}?#{params.collect { |k,v| "#{k}=#{v.to_s.gsub(" ","+")}"}.join("&")}").read)
     end
   end
 end
